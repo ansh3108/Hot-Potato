c8eb25f0be6f2225dab86222896d02b61e0fe8bf
Ansh Kumar <anshkumar9686@gmail.com>
2024-11-01 10:21:07 +0530

    code revision

--- script.js
+++ script.js
@@ -1,57 +1,50 @@
 let countdown;
 let timeLeft=10;
 const timeDisplay=document.getElementById("time-left");
-const consequenceDisplay =document.getElementById("consequence");
-let consequences=[];
+const consequenceDisplay=document.getElementById("consequence");
 const passPotatoButton=document.getElementById("pass-potato-button");
+let consequences=[];
 
 async function loadConsequences() {
     try{
         const response=await fetch('consequences.json');
         const data=await response.json();
         consequences=data.consequences;
+        console.log("Loaded consequences! ",consequences);
     } catch(error){
-        console.error("Failed to load consequences: ",error);
+        console.error("Failed to load consequences: ". error);
     }
 }
 
 function startTimer(){
-    timeLeft=Math.floor(Math.random()*6 )+5; //random time btw 5 and 10 seconds
+    console.log("Timer started");
+    timeLeft=Math.floor(Math.random()*6)+5; //random time btw 5 and 10 seconds
     timeDisplay.textContent=timeLeft;
-
-    countdown=setInterval( () => {
+    
+    countdown=setInterval(() => {
+        console.log("Running timer interval");
         timeLeft-=1;
         timeDisplay.textContent=timeLeft;
+        console.log("Time left: ",timeLeft);
 
-        if(timeLeft <=0){
+        if(timeLeft<=0){
             clearInterval(countdown);
             triggerConsequence();
         }
     }, 1000);
 }
 
-function triggerConsequence(){
-    if(consequences.length >0){
-        const randomConsequence=consequences[Math.floor(Math.random()*consequences.length)];
-        consequenceDisplay.textContent = `Consequence: ${randomConsequence}`;
-        consequenceDisplay.classList.remove("hidden"); //show consequence
-    } else{
-        consequenceDisplay.textContent="Cannot load consequences!";
-        consequenceDisplay.classList.remove("hidden");
-    }        
-}        
-
 function hideConsequence(){
     consequenceDisplay.classList.add("hidden");
 }
 
-passPotatoButton.addEventListener("click", () => {
-    console.log("Pass teh potato button clicked");
+passPotatoButton.addEventListener("click",() => {
+    console.log("Pass the potato button clicked");
     hideConsequence();
     startTimer();
-})
+});
 
 window.onload=async function(){
     await loadConsequences();
-    startTimer()
+    startTimer();
 }
